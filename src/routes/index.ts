588dc18e52695ec7ba9d24f72010ebbb150b76cc
Margub Murshed <murshedmargub@gmail.com>
2025-08-31 01:52:32 +0600

    Project initialized

---
+++ src/routes/index.ts
@@ -0,0 +1,6 @@
+import Landing from "@/pages/Landing";
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+    { index: true, Component: Landing }
+])
